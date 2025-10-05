14c2673b0ee3bda5ff26b6e64dbc4c951ccb56fc
hashirama <cosies-fades-0@icloud.com>
2025-10-05 21:52:38 +0300

    obsid many parts

--- DSP/trend3.py
+++ DSP/trend3.py
@@ -50,7 +50,7 @@ def find_exp_trend(df):
 
 def dft(df):
 	time_data = df['timestamp']
-	sampling_rate = 1 / (time_data[1] - time_data[0]).total_seconds()
+	sampling_rate = 1 / (time_data.iloc[1] - time_data.iloc[0]).total_seconds()
 	n = len(df)
 	dft_result = np.fft.fft(df['value'])
 	frequencies = np.fft.fftfreq(n, 1/sampling_rate)
@@ -72,54 +72,83 @@ current_df = pd.read_csv(filename,
 current_df['timestamp'] = pd.to_datetime(current_df['timestamp'])
 sensor_name = current_df['sensor_name'][0]
 
-current_df['value'] = current_df['value'].astype(float)
-current_df['value'] = current_df['value'].rolling(window=31, center=True, min_periods=1).mean()
-
-y_predicted, A_fit, tau_fit, C_fit = find_exp_trend(current_df)
+parts = np.array_split(current_df, 20)
 
 plt.figure(figsize=(12, 8))
-plt.xlabel('Время')
-plt.ylabel('Значение')
-plt.title(f'Экспоненциальная подгонка: {sensor_name}\n'
-		f'y = {A_fit:.3f} * exp(-t/{tau_fit:.3f}) + {C_fit:.3f}')
-plt.grid(True, alpha=0.3)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.plot(current_df['timestamp'], current_df['value'], 'bo-', 
-			alpha=0.7, label='Исходные данные', markersize=4)
-plt.plot(current_df['timestamp'], y_predicted, 'r-', 
-			linewidth=2, label='Экспоненциальная модель')
-plt.legend()
+colors = [
+    '#FF0000',  # ярко-красный
+    '#0000FF',  # ярко-синий  
+    '#00FF00',  # ярко-зеленый
+    '#FFA500',  # оранжевый
+    '#800080',  # фиолетовый
+    '#FF00FF',  # маджента
+    '#00FFFF',  # циан
+    '#FFFF00',  # желтый
+    '#FF4500',  # красно-оранжевый
+    '#008000',  # темно-зеленый
+    '#000080',  # темно-синий
+    '#800000',  # темно-красный
+    '#808000',  # оливковый
+    '#008080',  # бирюзовый
+    '#800080',  # пурпурный
+    '#FF1493',  # глубокий розовый
+    '#00FF7F',  # весенне-зеленый
+    '#FFD700',  # золотой
+    '#DC143C',  # малиновый
+    '#4682B4'   # стальной синий
+]
 
-untrended_df = current_df.copy()
-untrended_df['value'] = current_df['value'] - y_predicted
-untrended_df['value'] = untrended_df['value'] - untrended_df['value'].mean()
+for i, part in enumerate(parts): 
+	current_df = pd.DataFrame(part)
+	current_df['value'] = current_df['value'].astype(float)
+	# current_df['value'] = current_df['value'].rolling(window=1, center=True, min_periods=1).mean()
 
-plt.figure(figsize=(12, 8))
-plt.plot(untrended_df['timestamp'], untrended_df['value'], 'bo-', 
-			alpha=0.7, label='Вычтенный тренд', markersize=4)
-plt.legend()
-plt.xlabel('Время')
-plt.ylabel('Значение')
-plt.title(f'Вычтенный тренд')
-plt.grid(True, alpha=0.3)
-plt.xticks(rotation=45)
-plt.tight_layout()
+	y_predicted, A_fit, tau_fit, C_fit = find_exp_trend(current_df)
 
-magnitude, frequencies_half = dft(untrended_df)
-magnitude_filtered = pd.Series(magnitude).rolling(window=7, center=True, min_periods=1).mean()
+	# plt.figure(figsize=(12, 8))
+	# plt.xlabel('Время')
+	# plt.ylabel('Значение')
+	# plt.title(f'Экспоненциальная подгонка: {sensor_name}\n'
+	# 		f'y = {A_fit:.3f} * exp(-t/{tau_fit:.3f}) + {C_fit:.3f}')
+	# plt.grid(True, alpha=0.3)
+	# plt.xticks(rotation=45)
+	# plt.tight_layout()
+	# plt.plot(current_df['timestamp'], current_df['value'], 'bo-', 
+	# 			alpha=0.7, label='Исходные данные', markersize=4)
+	# plt.plot(current_df['timestamp'], y_predicted, 'r-', 
+	# 			linewidth=2, label='Экспоненциальная модель')
+	# plt.legend()
 
-magnitude_db = 20 * np.log10(magnitude)  
-magnitude_filtered_db = 20 * np.log10(magnitude_filtered)  
+	untrended_df = current_df.copy()
+	untrended_df['value'] = current_df['value'] - y_predicted
+	untrended_df['value'] = untrended_df['value'] - untrended_df['value'].mean()
 
-plt.figure(figsize=(12, 8))
-plt.plot(frequencies_half, magnitude_db, 'm-', linewidth=1, alpha=0.4, label='Исходная')
-plt.plot(frequencies_half, magnitude_filtered_db, 'g-', linewidth=1, alpha=0.8, label='Сглаженная')
-plt.legend()
-plt.xlabel('Частота (Гц)')
-plt.ylabel('Амплитуда (дБ)')
-plt.title('Амплитудный спектр')
-plt.grid(True, alpha=0.3)
-plt.xlim(0, min(1, frequencies_half[-1]))
+	# plt.figure(figsize=(12, 8))
+	# plt.plot(untrended_df['timestamp'], untrended_df['value'], 'bo-', 
+	# 			alpha=0.7, label='Вычтенный тренд', markersize=4)
+	# plt.legend()
+	# plt.xlabel('Время')
+	# plt.ylabel('Значение')
+	# plt.title(f'Вычтенный тренд')
+	# plt.grid(True, alpha=0.3)
+	# plt.xticks(rotation=45)
+	# plt.tight_layout()
+
+	magnitude, frequencies_half = dft(untrended_df)
+	magnitude_filtered = pd.Series(magnitude).rolling(window=7, center=True, min_periods=1).mean()
+
+	magnitude_db = 20 * np.log10(magnitude)  
+	magnitude_filtered_db = 20 * np.log10(magnitude_filtered)  
+
+	color = colors[i % len(colors)]
+
+	plt.plot(frequencies_half, magnitude_db, color=color, linewidth=1, alpha=0.4, label='Исходная')
+	# plt.plot(frequencies_half, magnitude_filtered_db, color="grey", linewidth=1, alpha=0.5, label='Сглаженная')
+	# plt.legend()
+	plt.xlabel('Частота (Гц)')
+	plt.ylabel('Амплитуда (дБ)')
+	plt.title('Амплитудный спектр')
+	plt.grid(True, alpha=0.3)
+	plt.xlim(0, min(1, frequencies_half[-1]))
 
 plt.show()
